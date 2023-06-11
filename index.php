4026884c91ef133058b3ea49fb94d3ae1d9ee4aa
Kanan Karizma <kowa9995@gmail.com>
2023-06-11 14:53:09 +0200

    CODE page index.php finished v1

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Garage V. Parrot</title>
 
   <!-- BOOTSTRAP  -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- FONT AWESOME  -->
   <script src="https://kit.fontawesome.com/1a0b88a9d7.js" crossorigin="anonymous"></script>
 
   <!-- CSS  -->
   <!-- <link rel="stylesheet" href="./assets/css/override-bootstrap.css"> -->
-  <link text="text/css" rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
@@ -24,7 +24,7 @@
     <header class="header">
       <nav class="nav">
         <a class="logo" href="/">
-          <img src="./assets/images/logo_car_title.png" />
+          <img src="./assets/images/logo_car_title.png" alt="logo garage parrot">
         </a>
         <div class="mobile-menu">
           <div class="line1"></div>
@@ -123,7 +123,7 @@
         <article class="cards">
           <div class="card">
             <div class="card-header">
-              <img class="card-img-top" src="./assets/images/car_hero.png" alt="" />
+              <img class="card-img-top" src="./assets/images/car_hero.png" alt="voiture marque X">
             </div>
             <div class="card-body">
               <h4 class="card-title">Title</h4>
@@ -131,14 +131,14 @@
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Nesciunt, quidem.
               </p>
-              <hr />
+              <hr>
               <p class="price">4444€</p>
               <a href="#" class="btn-fill large">Details</a>
             </div>
           </div>
           <div class="card">
             <div class="card-header">
-              <img class="card-img-top" src="./assets/images/car_hero.png" alt="" />
+              <img class="card-img-top" src="./assets/images/car_hero.png" alt="voiture marque Y">
             </div>
             <div class="card-body">
               <h4 class="card-title">Title</h4>
@@ -146,14 +146,14 @@
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Nesciunt, quidem.
               </p>
-              <hr />
+              <hr>
               <p class="price">4444€</p>
               <a href="#" class="btn-fill large">Details</a>
             </div>
           </div>
           <div class="card">
             <div class="card-header">
-              <img class="card-img-top" src="./assets/images/car_hero.png" alt="" />
+              <img class="card-img-top" src="./assets/images/car_hero.png" alt="voiture marque D">
             </div>
             <div class="card-body">
               <h4 class="card-title">Title</h4>
@@ -161,7 +161,7 @@
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Nesciunt, quidem.
               </p>
-              <hr />
+              <hr>
               <p class="price">4444€</p>
               <a href="#" class="btn-fill large">Details</a>
             </div>
@@ -257,25 +257,25 @@
             <div class="contact-form-left">
               <div>
                 <label for="lastname">Nom</label>
-                <input type="text" name="lastname" id="lastname" />
+                <input type="text" name="lastname" id="lastname">
               </div>
               <div>
                 <label for="name">Prenom</label>
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name">
               </div>
               <div>
                 <label for="email">Adresse email</label>
-                <input type="text" name="email" id="email" />
+                <input type="text" name="email" id="email">
               </div>
               <div>
                 <label for="phone">Téléphone</label>
-                <input type="text" name="phone" id="phone" />
+                <input type="text" name="phone" id="phone">
               </div>
             </div>
             <div class="contact-form-right">
               <div>
                 <label for="subject">Sujet</label>
-                <input type="text" name="subject" id="subject" />
+                <input type="text" name="subject" id="subject">
               </div>
               <div>
                 <label for="message">Message</label>
@@ -292,6 +292,123 @@
     </section>
     <!-- END CONTACT  -->
 
+    <!-- FOOTER   -->
+
+    <footer id="footer" class="footer">
+      <div class="footer-logo">
+        <img src="./assets/images/logo_white.png" alt="logo garage parrot">
+      </div>
+      <div class="footer-schedule">
+        <div class="footer-local">
+          <p class="footer-local-owner">Vicent Parrot</p>
+          <div class="footer-phone">
+            <i class="fa-solid fa-phone icon"></i>
+            <p>555-555-5455</p>
+          </div>
+          <div class="footer-email">
+            <i class="fa-solid fa-envelope icon"></i>
+            <p>garageparrot@email.com</p>
+          </div>
+          <div class="footer-address">
+            <p>45 Av. Gen. Foch 31000 Toulouse</p>
+          </div>
+        </div>
+        <div class="footer-days">
+          <h5 class="footer-days-title">Horaires d'ouverture</h5>
+          <div class="footer-day-week">
+            <span class="footer-day">lun:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">mar:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">mer:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">jeu:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">ven:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">sam:</span>
+            <span class="morning-start">08:45</span>
+            <span class="morning-end">12:00</span>
+            <span> -</span>
+            <span class="afternoon-start">14:00</span>
+            <span class="afternoon-end">18:00</span>
+          </div>
+          <div class="footer-day-week">
+            <span class="footer-day">dim:</span>
+            <span class="morning-start">ferme</span>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="footer-navigation">
+        <nav class="footer-nav">
+
+          <ul class="footer-nav-list">
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#services">Services</a>
+            </li>
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#cars">Voitures</a>
+            </li>
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#">Avis</a>
+            </li>
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#">Horaires</a>
+            </li>
+            <li>
+              <a class="footer-nav-link" aria-current="page" href="#">Contact</a>
+            </li>
+
+            <li>
+              <a href="#" class="footer-nav-link">Connecter</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="footer-social-network">
+          <i class="fa-brands fa-square-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-square-twitter"></i>
+        </div>
+      </div>
+    </footer>
+
+    <!-- END FOOTER   -->
+
 
   </main>
 
@@ -304,7 +421,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
   </script>
-  <script type="text/javascript" src="./assets/scripts/scripts.js"></script>
+  <script src="./assets/scripts/scripts.js"></script>
 </body>
 
 </html>
