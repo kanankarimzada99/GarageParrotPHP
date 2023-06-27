1dd57b9d9970adb1bcdbb8367830ec5ea1d26411
Kanan Karizma <kowa9995@gmail.com>
2023-06-27 11:55:43 +0200

    CODE login and logout pages

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__.'/lib/config.php';
+require __DIR__.'/lib/session.php';
+
+//replace the current session ID with a new one to stop session hijacking and session fixation.
+
+session_regenerate_id(true);
+
+//delete session data from the server
+session_destroy();
+
+//delete all information from $_SESSION table
+unset($_SESSION);
+
+//move to login page
+header ('location:login.php');
