555d131ed4d6e221e28299b54fb1a4f2af483d67
Kanan Karizma <kowa9995@gmail.com>
2023-06-28 09:17:37 +0200

    change password

--- lib/config.php
+++ lib/config.php
@@ -4,5 +4,5 @@ define("_CARS_IMAGES_FOLDER_", '/uploads/images');
 define("_ADMIN_ITEM_PER_PAGE_",10);
 define("_DB_NAME_",'garage_parrot');
 define("_DB_USER_",'root');
-define("_DB_PASSWORD_",'Marcos2023dev#');
+define("_DB_PASSWORD_",'');
 define("_APP_EMAIL_",'meneghettimarcos@outlook.com');
